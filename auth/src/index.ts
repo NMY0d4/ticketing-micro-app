0634bb312e828b2c0f6693680a658c351d99a6e5
Grégory <greg.marini@hotmail.fr>
2023-03-14 14:43:22 +0100

    finalization of error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
@@ -17,9 +17,8 @@ app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
 
-app.all('*', () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
-
 });
 
 app.use(errorHandler);
