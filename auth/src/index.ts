945af265398cfe22f19db7f5e4371ca793f0da64
Grégory <greg.marini@hotmail.fr>
2023-03-14 13:34:54 +0100

    add route not found error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -15,6 +16,11 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+
+});
 
 app.use(errorHandler);
 
