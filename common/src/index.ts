08a8e24898963d83ce4eaec5d255c1b8b24bde8a
Grégory Marini <greg.marini@hotmail.fr>
2023-03-24 11:13:32 +0100

    additional config

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 12,
+  blue: 22,
+  green: 41,
+};
+
+console.log(color);
+
+export default color;
