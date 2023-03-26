1da62d4e849bfdf832a0aad680106833736a8092
Grégory Marini <greg.marini@hotmail.fr>
2023-03-26 12:04:52 +0200

    build ticket model

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -27,7 +27,7 @@ it('returns an error if an invalid title is provided', async () => {
     .send({ title: '', price: 10 })
     .expect(400);
 
-    await request(app)
+  await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({ price: 10 })
@@ -41,11 +41,18 @@ it('returns an error if an invalid price is provided', async () => {
     .send({ title: 'title', price: -10 })
     .expect(400);
 
-    await request(app)
+  await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({ title: 'title' })
-    .expect(400);  
+    .expect(400);
 });
 
-it('creates a ticket with valid inputs', async () => {});
+it('creates a ticket with valid inputs', async () => {
+  // add in a check to make sure a ticket was saved
+
+  await request(app)
+    .post('/api/tickets')
+    .send({ title: 'title', price: 20 })
+    .expect(201);
+});
