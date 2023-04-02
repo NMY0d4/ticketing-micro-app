ab808eda19fc5bb56c10576faf6c0cba6ec880aa
Grégory Marini <greg.marini@hotmail.fr>
2023-04-02 17:25:59 +0200

    update test with mock for test publishing an event

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -53,12 +54,26 @@ it('creates a ticket with valid inputs', async () => {
   let tickets = await Ticket.find({});
   expect(tickets.length).toEqual(0);
 
+  const title = 'title';
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
-    .send({ title: 'title', price: 20 })
+    .send({ title, price: 20 })
     .expect(201);
 
   tickets = await Ticket.find({});
   expect(tickets.length).toEqual(1);
+});
+
+it('publishes an event', async () => {
+  const title = 'title';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({ title, price: 20 })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
