5dfe35c26913f669e03adb78c6f62f22440fc02e
Grégory Marini <greg.marini@hotmail.fr>
2023-04-05 13:56:54 +0200

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -15,3 +15,4 @@ export * from './events/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-events/ticket-created-event';
 export * from './events/ticket-events/ticket-updated-event';
+export * from './events/types/order-status';
